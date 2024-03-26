af8a5d8494bfeb4e53dd381c0ca511646acddc28
Pavani <vemasani.pavani@gmail.com>
2024-03-26 21:13:57 +0530

    create a infrastructure ,instance by using provisioners

--- Provisioners/main.tf
+++ Provisioners/main.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "test" {
-  ami = "ami-05f020f5935e52dc4"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [data.aws_instances.sec_group.id]
+  resource "aws_instance" "test" {
+    ami = "ami-05f020f5935e52dc4"
+    instance_type = "t3.small"
+    vpc_security_group_ids = [data.aws_instances.sec_group.id]
 
-  provisioner "remote-exec" {
-    inline = [
-       "sudo dnf install nginx",
-      "sudo systemctl start nginx"
-    ]
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = "DevOps321"
-      host     = self.public_ip
+    provisioner "remote-exec" {
+      inline = [
+        "sudo dnf install nginx",
+        "sudo systemctl start nginx"
+      ]
+      connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        password = "DevOps321"
+        host     = self.public_ip
+     }
     }
   }
-}
   data "aws_instances" "sec_group" {
-   name = "allow-all"
+    name = "allow-all"
   }
