b6b9cbeb3c7adc26b5e837f70c02aeffd3dd9846
Pavani <vemasani.pavani@gmail.com>
2024-04-02 10:34:38 +0530

    create a resource through module

--- app/main.tf
+++ app/main.tf
@@ -5,6 +5,18 @@ resource "aws_instance" "demo" {
     Name = var.tagsname
   }
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = self.public_ip
+    }
+    inline = [
+        "sudo dnf install nginx",
+        "sudo systemctl start nginx"
+    ]
+  }
 }
 
 data "aws_security_group" "selected"{
