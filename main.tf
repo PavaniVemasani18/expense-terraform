162a751310f8cbc2776f0535e586125335e48952
Pavani <vemasani.pavani@gmail.com>
2024-04-02 22:01:52 +0530

    create mysql module

--- main.tf
+++ main.tf
@@ -22,17 +22,17 @@ module "backend" {
   dnsrecordType = var.dnsType
   hostZone = var.hostZoneId
 }
-//module "mysql" {
-//  source = "modules/app"
-//  envr = var.env
-//  ami = var.ami
-//  component="backend"
-//  instanceType=var.instance_type
-//  type = var.type
-//  user = var.user
-//  password = var.password
-//  dnsrecordType = var.dnsType
-//  hostZone = var.hostZoneId
-//}
+module "mysql" {
+  source = "modules/app"
+  envr = var.env
+  ami = var.ami
+  component="backend"
+  instanceType=var.instance_type
+  type = var.type
+  user = var.user
+  password = var.password
+  dnsrecordType = var.dnsType
+  hostZone = var.hostZoneId
+}
 
 
