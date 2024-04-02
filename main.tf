87c79bf5e2cd69156a7c898d6417d612b86e383d
Pavani <vemasani.pavani@gmail.com>
2024-04-02 21:33:36 +0530

    create backend module

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "frontend" {
-  source = "./app"
+  source = "modules/app"
   envr = var.env
   ami = var.ami
   component="frontend"
@@ -10,5 +10,29 @@ module "frontend" {
   dnsrecordType = var.dnsType
   hostZone = var.hostZoneId
 }
+module "backend" {
+  source = "modules/app"
+  envr = var.env
+  ami = var.ami
+  component="backend"
+  instanceType=var.instance_type
+  type = var.type
+  user = var.user
+  password = var.password
+  dnsrecordType = var.dnsType
+  hostZone = var.hostZoneId
+}
+//module "mysql" {
+//  source = "modules/app"
+//  envr = var.env
+//  ami = var.ami
+//  component="backend"
+//  instanceType=var.instance_type
+//  type = var.type
+//  user = var.user
+//  password = var.password
+//  dnsrecordType = var.dnsType
+//  hostZone = var.hostZoneId
+//}
 
 
