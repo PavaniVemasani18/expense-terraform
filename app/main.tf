83c7d72a0297f03715e3a2488dfbb47d3db5f9c4
Pavani <vemasani.pavani@gmail.com>
2024-04-02 14:54:04 +0530

    mispelled word

--- app/main.tf
+++ app/main.tf
@@ -16,21 +16,12 @@ resource "null_resource" "expense" {
       host     = aws_instance.instance.public_ip
     }
     inline = [
-//      "sudo  dnf pip3.11 install ansible",
-//      "ansible-pull -i localhost, -u https://github.com/PavaniVemasani18/learn-ansible.git expense.yml -e ${var.envr} -e rolename=${var.component} "
-     "sudo dnf install nginx -y",
-      "sudo systemctl start service"
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
     ]
   }
 }
 
-//resource "aws_route53_record" "dnsrecord" {
-//  name = "${var.component} - ${var.envr}"
-//  type = "A"
-//  zone_id = var.route_zoneid
-//  ttl = var.dnsrecordttl
-//  records = [aws_instance.instance.private_ip]
-//}
 
 data "aws_security_group" "selected"{
   name = var.securityname
