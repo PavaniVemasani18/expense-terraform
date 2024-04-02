1e3d436266ecba0f369cab234e138423dac80849
Pavani <vemasani.pavani@gmail.com>
2024-04-02 11:26:14 +0530

    create a pipleline and run terraform through pipeline

--- app/main.tf
+++ app/main.tf
@@ -1,16 +1,27 @@
-resource "aws_instance" "demo" {
+resource "aws_instance" "frontend" {
   ami = var.ami
   instance_type = var.instanceType
   tags = {
     Name = var.tagsname
   }
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+resource "aws_instance" "backend" {
+  ami = var.ami
+  instance_type = var.instanceType
+  tags = {
+    Name = var.tagsname
+  }
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+
+resource "null_resource" "expense" {
   provisioner "remote-exec" {
     connection {
       type     = var.type
       user     = var.user
       password = var.password
-      host     = self.public_ip
+      host     = aws_instance.frontend.public_ip
     }
     inline = [
         "sudo dnf install nginx -y",
